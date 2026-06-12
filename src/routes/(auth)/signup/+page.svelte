632c5b596bdd7a4069a6d66990222c421b14eae9
Guillermo Garcia <125451487+guillermo-garciarey@users.noreply.github.com>
2026-06-12 20:01:03 +0100

    Auth pages styling

--- src/routes/(auth)/signup/+page.svelte
+++ src/routes/(auth)/signup/+page.svelte
@@ -1,15 +1,13 @@
 <script>
-	import GalleryVerticalEndIcon from '@lucide/svelte/icons/gallery-vertical-end';
 	import SignupForm from '$lib/components/signup-form.svelte';
+	import Book from '@lucide/svelte/icons/book-text';
 </script>
 
 <div class="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
 	<div class="flex w-full max-w-sm flex-col gap-6">
 		<a href="#/" class="flex items-center gap-2 self-center font-medium">
-			<div
-				class="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md"
-			>
-				<GalleryVerticalEndIcon class="size-4" />
+			<div class="bg-transparent text-primary flex size-6 items-center justify-center rounded-md">
+				<Book class="size-4" />
 			</div>
 			N365
 		</a>
